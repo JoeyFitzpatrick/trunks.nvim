243999174480bbecaf265e917c1d8c88974ba762
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-02-14 16:18:46 -0600

    feat: merge conflict keymaps

--- lua/ever/_ui/interceptors/mergetool/init.lua
+++ lua/ever/_ui/interceptors/mergetool/init.lua
@@ -1,6 +1,8 @@
 local M = {}
 
-local function get_conflict_lines()
+local KEYMAPS_ARE_SET = false
+
+local function get_all_conflict_lines()
     local run_cmd = require("ever._core.run_cmd").run_cmd
 
     -- Get the initial list of potential conflict lines
@@ -39,7 +41,7 @@ local function populate_and_open_quickfix()
 
     -- Prepare the entries for the quickfix list
     local qf_entries = {}
-    for _, item in ipairs(get_conflict_lines()) do
+    for _, item in ipairs(get_all_conflict_lines()) do
         local filename, lnum, text = item:match("([^:]+):(%d+): (.+)")
         table.insert(qf_entries, {
             filename = filename,
@@ -55,8 +57,57 @@ local function populate_and_open_quickfix()
     vim.cmd("copen")
 end
 
+local function get_local_conflict_lines()
+    local start_line = vim.fn.search("^<<<<<<< HEAD", "bcnW")
+    local end_line = vim.fn.search("^>>>>>>> ", "nW")
+
+    if start_line == 0 or end_line == 0 then
+        return nil
+    end
+
+    return vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+end
+
+---@param strategy "base" | "ours" | "theirs" | "all"
+local function replace_conflict(strategy)
+    local conflict_lines = get_local_conflict_lines()
+
+    if not conflict_lines then
+        print("No merge conflict found at cursor position.")
+        return
+    end
+
+    local resolved_lines = require("ever._ui.interceptors.mergetool.handle_merge_conflict").resolve_merge_conflict(
+        conflict_lines,
+        strategy
+    )
+    local start_line = vim.fn.search("^<<<<<<< HEAD", "bcnW")
+    local end_line = vim.fn.search("^>>>>>>> ", "nW")
+
+    vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, resolved_lines)
+end
+
+local function set_keymaps()
+    vim.keymap.set("n", "<Plug>(Ever-resolve-base)", function()
+        replace_conflict("base")
+    end, { noremap = true, silent = true })
+    vim.keymap.set("n", "<Plug>(Ever-resolve-ours)", function()
+        replace_conflict("ours")
+    end, { noremap = true, silent = true })
+    vim.keymap.set("n", "<Plug>(Ever-resolve-theirs)", function()
+        replace_conflict("theirs")
+    end, { noremap = true, silent = true })
+    vim.keymap.set("n", "<Plug>(Ever-resolve-all)", function()
+        replace_conflict("all")
+    end, { noremap = true, silent = true })
+end
+
 function M.render()
     populate_and_open_quickfix()
+    if not KEYMAPS_ARE_SET then
+        set_keymaps()
+        KEYMAPS_ARE_SET = true
+    end
 end
 
 return M
