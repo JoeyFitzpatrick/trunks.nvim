13158b758fa9de3bc5bbde11cff3c7f5ffcd8c77
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-02-27 22:35:04 -0600

    chore: create buffers for grep qflist

--- lua/ever/_ui/interceptors/grep/init.lua
+++ lua/ever/_ui/interceptors/grep/init.lua
@@ -24,9 +24,47 @@ function M._create_grep_qflist_item(bufnr, line)
     }
 end
 
+---@param line string
+local function create_qflist_item_buffer(line)
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_name(bufnr, line:match(".+:"))
+    return bufnr
+end
+
 ---@param cmd string
 function M.render(cmd)
-    print(add_grep_flags(cmd))
+    cmd = add_grep_flags(cmd)
+    -- Action should be nil at first to create new qflist
+    local action = " "
+    local function on_stdout(_, data, _)
+        if data then
+            for _, line in ipairs(data) do
+                local bufnr = create_qflist_item_buffer(line)
+                vim.fn.setqflist({ M._create_grep_qflist_item(bufnr, line) }, action)
+                if action == " " then
+                    action = "a"
+                end
+            end
+        end
+    end
+
+    local function on_exit(code)
+        if code ~= 0 then
+            vim.notify("command '" .. cmd .. "' failed with exit code " .. code, vim.log.levels.ERROR)
+        else
+            vim.cmd("copen")
+        end
+    end
+
+    -- Start the asynchronous job
+    vim.fn.jobstart(cmd, {
+        on_stdout = function(...)
+            pcall(on_stdout, ...)
+        end,
+        on_exit = function(_, code, _)
+            pcall(on_exit, code)
+        end,
+    })
 end
 
 return M
