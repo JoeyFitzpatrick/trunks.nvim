5c2086ab99324ecec3040c3ee6e1527c667ac475
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-02-28 22:15:02 -0600

    chore: some grep quickfix stuff

--- lua/ever/_ui/interceptors/grep/init.lua
+++ lua/ever/_ui/interceptors/grep/init.lua
@@ -41,31 +41,35 @@ end
 ---@param line string
 local function create_qflist_item_buffer(line)
     local bufnr = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_name(bufnr, line:match(".+:"))
+    local parsed_line = parse_grep_output_line(line)
+    vim.api.nvim_buf_set_name(
+        bufnr,
+        string.format(
+            "%s/%s%s",
+            os.tmpname(),
+            require("ever._constants.constants").FILENAME_PREFIX,
+            parsed_line.filename
+        )
+    )
     return bufnr
 end
 
 ---@param cmd string
 function M.render(cmd)
     cmd = add_grep_flags(cmd)
-    local buffer_render_fns = {}
-    -- Action should be nil at first to create new qflist
+    -- Action should be " " at first to create new qflist
     local action = " "
+    vim.cmd("copen")
     local function on_stdout(_, data, _)
         if data then
             for _, line in ipairs(data) do
                 local bufnr = create_qflist_item_buffer(line)
                 local parsed_grep_output_line = parse_grep_output_line(line)
-                buffer_render_fns[bufnr] = vim.schedule_wrap(function()
-                    local lines = require("ever._core.run_cmd").run_cmd(
-                        string.format(
-                            "git show %s -- %s",
-                            parsed_grep_output_line.commit,
-                            parsed_grep_output_line.filename
-                        )
-                    )
-                    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-                end)
+                local lines = require("ever._core.run_cmd").run_cmd(
+                    string.format("git show %s:%s", parsed_grep_output_line.commit, parsed_grep_output_line.filename)
+                )
+                vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+                vim.api.nvim_set_option_value("filetype", vim.filetype.match({ buf = bufnr }), { buf = bufnr })
                 vim.fn.setqflist({ M._create_grep_qflist_item(bufnr, line) }, action)
                 if action == " " then
                     action = "a"
@@ -77,13 +81,6 @@ function M.render(cmd)
     local function on_exit(code)
         if code ~= 0 then
             vim.notify("command '" .. cmd .. "' failed with exit code " .. code, vim.log.levels.ERROR)
-        else
-            vim.cmd("copen")
-            vim.schedule(function()
-                for _, render_fn in pairs(buffer_render_fns) do
-                    render_fn()
-                end
-            end)
         end
     end
 
