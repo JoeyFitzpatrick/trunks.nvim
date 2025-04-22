ac0b08f4c80b63a2c278d6037ee6b7738ae844e0
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-04-22 15:41:00 -0500

    chore: add a test around stage_all

--- spec/ever/_ui/status/should_stage_files_spec.lua
+++ spec/ever/_ui/status/should_stage_files_spec.lua
@@ -3,13 +3,13 @@ local should_stage_files = require("ever._ui.home_options.status.status_utils").
 describe("should stage files", function()
     it("should return true if any files are unstaged", function()
         local files_with_unstaged_files = {
-            "R  lua/ever/_ui/home_options/status.lua -> lua/ever/_ui/home_options/status/init.lua",
-            "A  lua/ever/_ui/home_options/status/status_utils.lua",
-            "D lua/ever/_ui/home_options/test.lua",
+            " A  lua/ever/_ui/home_options/status/status_utils.lua",
+            " D lua/ever/_ui/home_options/test.lua",
             "?? spec/ever/_ui/status/should_stage_files_spec.lua",
         }
         assert.are.equal(true, should_stage_files(files_with_unstaged_files))
     end)
+
     it("should return false if all files are staged", function()
         local all_staged_files = {
             "R  lua/ever/_ui/home_options/status.lua -> lua/ever/_ui/home_options/status/init.lua",
@@ -19,6 +19,16 @@ describe("should stage files", function()
         }
         assert.are.equal(false, should_stage_files(all_staged_files))
     end)
+
+    it("should return true if files are partially staged", function()
+        local all_staged_files = {
+            "MM lua/ever/_ui/home_options/status/status_utils.lua",
+            "AM lua/ever/_ui/home_options/test.lua",
+            "A  spec/ever/_ui/status/should_stage_files_spec.lua",
+        }
+        assert.are.equal(true, should_stage_files(all_staged_files))
+    end)
+
     it("should return false if no files are given", function()
         assert.are.equal(false, should_stage_files({}))
     end)
