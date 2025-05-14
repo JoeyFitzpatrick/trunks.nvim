290808be4056b99a47afd241d6b6fbfea07103f8
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-05-13 23:45:55 -0500

    feat: help command improvements
    * Add highlighting by setting filetype to "man"
    * Stream in lines for performance
    * Use the whole window
    * Use a non-terminal buffer, so vim bindings work

--- lua/ever/_ui/interceptors/init.lua
+++ lua/ever/_ui/interceptors/init.lua
@@ -11,6 +11,16 @@ local cmd_ui_map = {
     difftool = function(cmd)
         require("ever._ui.interceptors.difftool").render(cmd)
     end,
+    help = function(cmd)
+        -- col -b is needed to remove bad characters from --help output
+        require("ever._ui.interceptors.standard_interceptor").render(cmd .. " | col -b")
+        -- Setting the filetype to man add nice highlighting.
+        -- It also makes the "q" keymap exit neovim if this is the last buffer, so we need to set it again
+        vim.bo["filetype"] = "man"
+        vim.keymap.set("n", "q", function()
+            require("ever._core.register").deregister_buffer(0, { skip_go_to_last_buffer = true })
+        end, { buffer = 0 })
+    end,
     log = function(cmd)
         local bufnr = require("ever._ui.elements").new_buffer({ buffer_name = "EverLog-" .. os.tmpname() })
         require("ever._ui.home_options.log").render(bufnr, { start_line = 0, cmd = cmd })
@@ -38,7 +48,7 @@ local standard_output_commands = {
 
 for _, command in ipairs(standard_output_commands) do
     cmd_ui_map[command] = function(cmd)
-        require("ever._ui.interceptors.standard_interceptor").render(cmd)
+        require("ever._ui.interceptors.standard_interceptor").render(cmd .. " | col -b")
     end
 end
 
@@ -53,6 +63,10 @@ function M.get_ui(cmd)
     end
     if cmd:match("^difftool%s*$") then
         return cmd_ui_map.staging_area
+    end
+    local is_help_command = cmd:match("%-h%s*$") or cmd:match("%-%-help%s*$")
+    if is_help_command then
+        return cmd_ui_map.help
     end
     return cmd_ui_map[subcommand]
 end
