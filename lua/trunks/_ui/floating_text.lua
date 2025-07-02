7be7b691e144c5cb7a5d6a3e436e645dc39260e7
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-07-01 23:25:20 -0500

    feat: remove keymaps helper text when window changes height

--- lua/trunks/_ui/floating_text.lua
+++ lua/trunks/_ui/floating_text.lua
@@ -1,6 +1,9 @@
 local M = {}
 
-local function show_bottom_floating_text(bufnr, win, text)
+---@param bufnr integer
+---@param win integer
+---@param text string
+local function render_floating_win(bufnr, win, text)
     local width = vim.api.nvim_win_get_width(win)
     local height = 1
     local row = vim.api.nvim_win_get_height(win) - 1
@@ -36,6 +39,40 @@ local function show_bottom_floating_text(bufnr, win, text)
         end,
         desc = "Close bottom floating text when buffer or window closes",
     })
+
+    return float_win
+end
+
+---@param bufnr integer
+---@param win integer
+---@param text string
+---@return integer
+local function show_bottom_floating_text(bufnr, win, text)
+    local function attach_resize_handler(float_win, row)
+        local augroup = vim.api.nvim_create_augroup("BottomFloatingText_" .. float_win, { clear = false })
+        vim.api.nvim_create_autocmd("WinResized", {
+            group = augroup,
+            callback = function()
+                if not vim.api.nvim_win_is_valid(win) then
+                    return
+                end
+                local new_row = vim.api.nvim_win_get_height(win) - 1
+                local win_valid = vim.api.nvim_win_is_valid(float_win)
+                if win_valid and new_row ~= row then
+                    vim.api.nvim_win_close(float_win, true)
+                end
+                if (not win_valid) and new_row == row and vim.api.nvim_buf_is_valid(bufnr) then
+                    local new_float_win = render_floating_win(bufnr, win, text)
+                    attach_resize_handler(new_float_win, new_row)
+                end
+            end,
+            desc = "Resize/move bottom floating text on window resize",
+        })
+    end
+
+    local row = vim.api.nvim_win_get_height(win) - 1
+    local float_win = render_floating_win(bufnr, win, text)
+    attach_resize_handler(float_win, row)
     return float_win
 end
 
