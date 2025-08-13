b4851c9e986992b5f5648536449e41904a0b8477
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-08-12 23:24:30 -0500

    chore: diff-qf line nums working, with tests

--- lua/trunks/_ui/trunks_commands/diff_qf.lua
+++ lua/trunks/_ui/trunks_commands/diff_qf.lua
@@ -1,5 +1,5 @@
 ---@class trunks.DiffQfHunk
----@field filename string
+---@field bufnr integer
 ---@field line_nums integer[]
 
 local M = {}
@@ -15,61 +15,91 @@ local function parse_filename(line)
     return nil
 end
 
----@param hunk string[]
 ---@param filename? string
----@return trunks.DiffQfHunk
-function M._parse_diff_line(hunk, filename)
-    local i = 1
+---@param commit_range? string
+---@return integer -- bufnr of created buffer
+function M._create_buffer(filename, commit_range)
+    return -1
+end
 
-    -- Before "@@" line
-    while not vim.startswith(hunk[i], "@@") do
-        if not filename then
-            filename = parse_filename(hunk[i])
-        end
-        i = i + 1
+---@param lines string[]
+---@param commit_range? string
+---@return trunks.DiffQfHunk[]
+function M._parse_diff_output(lines, commit_range)
+    local qf_locations = {}
+    local function get_initial_state()
+        return {
+            filename = nil,
+            line_nums = {},
+            start_line = nil,
+            num_lines_since_start = 0,
+            should_use_line = true,
+            hunk_added_lines = false,
+            finalized_line_num = false,
+        }
     end
+    local state = get_initial_state()
 
-    -- At "@@" line
-    local start_line = tonumber(hunk[i]:match("%+(%d+)"))
-    i = i + 1
+    for i, line in ipairs(lines) do
+        if vim.startswith(line, "diff") and i ~= 1 then
+            table.insert(
+                qf_locations,
+                { bufnr = M._create_buffer(state.filename, commit_range), line_nums = state.line_nums }
+            )
+            state = get_initial_state()
+        elseif vim.startswith(line, "---") or vim.startswith(line, "+++") then
+            state.filename = line:sub(7) -- Everything after diff text, e.g. "--- a/"
+        elseif vim.startswith(line, "@@") then
+            state.start_line = tonumber(line:match("%+(%d+)"))
+            state.num_lines_since_start = 0
+        elseif state.start_line then
+            local is_removed_line = vim.startswith(line, "-")
+            local is_added_line = vim.startswith(line, "+")
+            local is_changed_line = is_removed_line or is_added_line
+            -- Only add first changed line in each group of changed lines.
+            -- A hunk can have multiple groups of changed lines.
+            if state.should_use_line and is_changed_line then
+                table.insert(state.line_nums, state.num_lines_since_start + state.start_line)
+                state.should_use_line = false
+                state.finalized_line_num = false
+            elseif not is_changed_line then
+                state.should_use_line = true
 
-    -- After @@ line
-    local line_nums = {}
-    local num_lines_since_start = 0
-    local should_use_line = true
-    while i <= #hunk do
-        local line = hunk[i]
-        local is_changed_line = vim.startswith(line, "-") or vim.startswith(line, "+")
+                if not state.finalized_line_num and not state.hunk_added_lines and #state.line_nums > 0 then
+                    state.line_nums[#state.line_nums] = state.line_nums[#state.line_nums] - 1
+                end
+                state.finalized_line_num = true
+                state.hunk_added_lines = false
+            end
 
-        -- Only add first changed line in each group of changed lines.
-        -- A hunk can have multiple groups of changed lines.
-        if should_use_line and is_changed_line then
-            table.insert(line_nums, num_lines_since_start + start_line)
-            should_use_line = false
-        elseif not is_changed_line then
-            should_use_line = true
+            if not is_removed_line then
+                state.num_lines_since_start = state.num_lines_since_start + 1
+            end
+
+            if is_added_line then
+                state.hunk_added_lines = true
+            end
         end
-
-        i = i + 1
-        num_lines_since_start = num_lines_since_start + 1
     end
-    return { filename = filename, line_nums = line_nums }
+    if #state.line_nums > 0 then
+        table.insert(
+            qf_locations,
+            { bufnr = M._create_buffer(state.filename, commit_range), line_nums = state.line_nums }
+        )
+    end
+    return qf_locations
 end
 
 ---@param commit_range? string
 local function get_qf_locations(commit_range)
     local cmd = commit_range and ("diff " .. commit_range) or "diff"
     local command_builder = Command.base_command(cmd)
+    local diff_output = require("trunks._core.run_cmd").run_cmd(command_builder)
 
-    local locations = {}
-    vim.fn.jobstart(command_builder:build(), {
-        on_stdout = function(_, data, _)
-            if data then
-                for i, line in ipairs(data) do
-                end
-            end
-        end,
-    })
+    local qf_locations = M._parse_diff_output(diff_output, commit_range)
+
+    local bufnr = require("trunks._ui.elements").new_buffer({})
+    require("trunks._ui.utils.buffer_text").set(bufnr, qf_locations)
 end
 
 ---@param commit_range? string
--- spec/trunks/_ui/trunks_commands/diff_qf.lua
+++ spec/trunks/_ui/trunks_commands/diff_qf.lua
@@ -1,63 +1,180 @@
-describe("diff-qf diff line parser", function()
-    local parse_diff_line = require("trunks._ui.trunks_commands.diff_qf")._parse_diff_line
-    it("returns a file location in a git diff output line", function()
-        local result = parse_diff_line({
+describe("diff-qf diff output parser", function()
+    local parse_diff_output = require("trunks._ui.trunks_commands.diff_qf")._parse_diff_output
+
+    before_each(function()
+        require("trunks._ui.trunks_commands.diff_qf")._create_buffer = function(filename, commit_range)
+            return -1
+        end
+    end)
+
+    it("returns a file locations for multiple hunks", function()
+        local result = parse_diff_output({
             "diff --git c/README.md w/README.md",
             "index ce51519..24439ed 100644",
             "--- c/README.md",
             "+++ w/README.md",
             "@@ -102,6 +102,7 @@ Note: lazy loading is handled internally, so it is not required to lazy load Tru",
-            ' commit_amend_reuse_message = "A",',
-            ' commit_dry_run = "d",',
-            ' commit_no_verify = "n",',
+            '                     commit_amend_reuse_message = "A",',
+            '                     commit_dry_run = "d",',
+            '                     commit_no_verify = "n",',
             '+                    commit_instant_fixup = "F", -- Run :Trunks commit-instant-fixup',
-            " },",
-            " },",
-            " diff = {",
+            "                 },",
+            "             },",
+            "             diff = {",
+            "diff --git c/doc/roadmap_to_beta.md w/doc/roadmap_to_beta.md",
+            "index 868a119..9eef73e 100644",
+            "--- c/doc/roadmap_to_beta.md",
+            "+++ w/doc/roadmap_to_beta.md",
+            "@@ -5,9 +5,6 @@ While the project is in alpha, we will not use versioning. Once all of the impro",
+            " ",
+            " ## Features",
+            " ",
+            "-### Display files in a tree",
+            "-In all UIs that display files, we currently display all files in a flat structure; each file takes up one line. It would be great to be able to display files in a tree instead. This should be config driven. This should be doable for most, if not all, UIs. The first one to tackle would be the status UI.",
+            "-",
+            " ### Undo",
+            " Not well thought out, but the idea is that we could undo some operations:",
+            " * undo a merge commit (just reset to the commit before the merge probably)",
+            "@@ -40,20 +37,31 @@ Users should be able to create their own keymaps for different UIs. This can be",
+            " A potential pitfall here is that we currently map from command to key, not key to map. So we'd either need to invert existing keymaps, or support current mappings but allow new ones to be inverted.",
+            " 2. Create autocmds for different UIs, so that keymaps could be created for a given UI as part of its autocmd. Not a huge fan of this approach, but there might be other reasons to create these autocmds anyways.",
+            " ",
+            "+### Quickfix list integration",
+            "+Some ideas for quickfix list integrations that would be helpful:",
+            "+",
+            "+- Diffs could populate the quickfix list with all changes, so that one could navigate between the changes easily. Would be helpful for inspecting diffs and code review. This can work by calling `git diff`, parsing diff locations, filling out the quickfix list with these, and open the file and vdiff it each time it's opened. Could probably convert `:G difftool` to this, but we'd need a way to stage/unstage hunks/lines from these vidiff'ed files.",
+            "+- Fugitive has a `:Gclog` command that would be nice to copy.",
+            "+- A git grep integration would be nice.",
+            "+- Mergetool already has quickfix list integration.",
+            "+- Question: do we need a single command for all quickfix list stuff, like `:Trunks qf {subcommand}` or `:Trunks qf-{subcommand}`, or would ad-hoc commands be better, like `:G grep` just working? Leaning towards a single command to be explicit.",
+            "+",
+            " ## Improvements",
+            " ",
+            " ### Diff highlight engine",
+            " I'm not a big fan of the built-in diff highlighting. It would be nice to have something closer to [delta](https://github.com/dandavison/delta). This would allow for using regular buffers for diffs in all cases, instead of terminal buffers, which would be a big win for functionality.",
+            " ",
+            "-I am pretty sure we can leverage virtual text to accomplish this. The gist of it would be:",
+            "+There is prior art that leverages virtual text to accomplish this, but only for diffs for a single file. The gist of it would be:",
+            " 1. Get diff output",
+            " 1. Omit the first character of each line, except the @@ lines (aka context lines)",
+            " 1. Highlight added/removed lines appropriately",
+            " 1. Omit the context lines entirely, and replace with virtual text using `vim.api.nvim_buf_set_extmark`",
+            " This should print the context lines, while not counting them for syntax highlighting purposes since they aren't actually in the buffer.",
+            " ",
+            "+Ideally, we'd be able to highlight diffs with multiple files with multiple file types. A rough idea is to use treesitter to highlight each part of the file as it's filetype, using nested treesitter query stuff. Not really sure how this works though.",
+            "+",
+            " ### Git pull can't rebase multiple branches",
+            "-Sometimes when using the `git pull` mapping in a UI, instead of pulling, there is an error that says `can't rebase onto multiple branches`. This usually goes away after pulling a second or third time. It would be nice if this just didn't happen at all.",
+            "+Sometimes when using the `git pull` mapping in a UI, instead of pulling, there is an error that says `can't rebase onto multiple branches`. This usually goes away after pulling a second or third time. It would be nice if this just didn't happen at all. I think this has something to do with `git fetch` or some other background command running, because sometimes this occurs even when I just open Trunks and use pull without switching branches.",
+            " ",
+            " ### Git log -S with multiline visual selection",
+            " It would be awesome if using `:G log -S` from visual mode worked with a multiline selection. One way this _could_ work is to use concepts from [this blog post](https://hoelz.ro/blog/applying-gits-pickaxe-option-across-multiple-lines-of-yaml-using-textconv). The gist of it is:",
+            "@@ -75,3 +83,6 @@ In git, many commands take a `--quiet` flag that surpresses informational messag",
+            " ### Reblame handles files not in commit",
+            " When reblaming a file that has a different name at a given commit, a pretty ugly error is shown. We should handle that more gracefully by either figuring out the old file name, or if that's not possible, show a better error.",
+            " ",
+            "+### Worktree UI improvements",
+            "+There are some weird things that happen with worktrees and our strategy to respect the current buffer as the git dir. When changing to a different worktree and running a git command, the git command behaves as though we’re still in the previous dir. Additionally, changing back to the original worktree doesn’t seem to change back to the correct branch.",
+            "+We definitely need a way to make worktrees not use the “respect buffer dir” logic, and we also need to see what’s going on with the branch changes not persisting.",
         })
 
-        local expected = { filename = "README.md", line_nums = { 105 } }
+        local expected = {
+            { bufnr = -1, line_nums = { 105 } },
+            { bufnr = -1, line_nums = { 7, 40, 54, 61, 64, 86 } },
+        }
         assert.are.same(expected, result)
     end)
 
-    it("returns a multiple file locations in a git diff output line", function()
-        local result = parse_diff_line({
-            "diff --git c/README.md w/README.md",
-            "index ce51519..24439ed 100644",
-            "--- c/README.md",
-            "+++ w/README.md",
-            "@@ -102,6 +102,7 @@ Note: lazy loading is handled internally, so it is not required to lazy load Tru",
-            ' commit_amend_reuse_message = "A",',
-            ' commit_dry_run = "d",',
-            ' commit_no_verify = "n",',
-            '+                    commit_instant_fixup = "F", -- Run :Trunks commit-instant-fixup',
-            " },",
-            "+},", -- This is a new added line
-            " diff = {",
+    it("returns a file locations for a complicated hunk", function()
+        local result = parse_diff_output({
+            "diff --git c/lua/trunks/_ui/popups/commit_popup.lua w/lua/trunks/_ui/popups/commit_popup.lua",
+            "index 8b6683f..e82e9f7 100644",
+            "--- c/lua/trunks/_ui/popups/commit_popup.lua",
+            "+++ w/lua/trunks/_ui/popups/commit_popup.lua",
+            "@@ -1,35 +1,61 @@",
+            " local M = {}",
+            " ",
+            '+local popup = require("trunks._ui.popups.popup")',
+            "+",
+            " ---@param bufnr integer",
+            "-local function set_keymaps(bufnr)",
+            "+---@param ui_type string",
+            "+---@return { basic: trunks.PopupMapping[], edit: trunks.PopupMapping[] }",
+            "+local function get_keymaps_with_descriptions(bufnr, ui_type)",
+            '     local keymaps = require("trunks._ui.keymaps.base").get_keymaps(bufnr, "commit_popup", { popup = true })',
+            "-    local keymap_opts = { noremap = true, silent = true, buffer = bufnr, nowait = true }",
+            '-    local set = require("trunks._ui.keymaps.set").safe_set_keymap',
+            '+    local descriptions = require("trunks._constants.keymap_descriptions").long_descriptions[ui_type]',
+            "+    local mappings = { basic = {}, edit = {} }",
+            " ",
+            "     local keymap_command_map = {",
+            '-        [keymaps.commit] = "G commit",',
+            '-        [keymaps.commit_amend] = "G commit --amend",',
+            '-        [keymaps.commit_amend_reuse_message] = "G commit --amend --reuse-message HEAD --no-verify",',
+            '-        [keymaps.commit_dry_run] = "G commit --dry-run",',
+            '-        [keymaps.commit_no_verify] = "G commit --no-verify",',
+            "+        basic = {",
+            '+            [keymaps.commit] = "G commit",',
+            '+            [keymaps.commit_amend] = "G commit --amend",',
+            '+            [keymaps.commit_amend_reuse_message] = "G commit --amend --reuse-message HEAD --no-verify",',
+            '+            [keymaps.commit_dry_run] = "G commit --dry-run",',
+            '+            [keymaps.commit_no_verify] = "G commit --no-verify",',
+            "+        },",
+            "+        edit = {",
+            '+            [keymaps.commit_instant_fixup] = "Trunks commit-instant-fixup",',
+            "+        },",
+            "     }",
+            " ",
+            "-    for keys, command in pairs(keymap_command_map) do",
+            '-        set("n", keys, function()',
+            "-            vim.api.nvim_buf_delete(bufnr, { force = true })",
+            "-            vim.cmd(command)",
+            "-        end, keymap_opts)",
+            "+    for name, keys in pairs(keymaps) do",
+            "+        if keys and keymap_command_map.basic[keys] then",
+            "+            table.insert(mappings.basic, {",
+            "+                keys = keys,",
+            "+                description = descriptions[name],",
+            "+                -- Yes, using keys as the key. Pretty disgusting and should be fixed.",
+            "+                action = keymap_command_map.basic[keys],",
+            "+            })",
+            "+        end",
+            "+        if keys and keymap_command_map.edit[keys] then",
+            "+            table.insert(mappings.edit, {",
+            "+                keys = keys,",
+            "+                description = descriptions[name],",
+            "+                action = keymap_command_map.edit[keys],",
+            "+            })",
+            "+        end",
+            "     end",
+            "+",
+            "+    return mappings",
+            " end",
+            " ",
+            " ---@return integer -- bufnr",
+            " function M.render()",
+            '-    local bufnr = require("trunks._ui.popups.popup").render_popup({',
+            '-        ui_type = "commit_popup",',
+            '+    local bufnr = require("trunks._ui.elements").new_buffer({',
+            '         buffer_name = "TrunksCommitPopup",',
+            '-        title = "Commit",',
+            '+        win_config = { split = "below" },',
+            "+    })",
+            "+",
+            '+    local maps = get_keymaps_with_descriptions(bufnr, "commit_popup")',
+            "+    popup.render(bufnr, {",
+            '+        { title = "Commit", rows = maps.basic },',
+            '+        { title = "Edit", rows = maps.edit },',
+            "     })",
+            "-    set_keymaps(bufnr)",
+            "     return bufnr",
+            " end",
+            " ",
         })
 
-        local expected = { filename = "README.md", line_nums = { 105, 107 } }
-        assert.are.same(expected, result)
-    end)
-
-    it("returns a single file location for a group of connected changes", function()
-        local result = parse_diff_line({
-            "diff --git c/README.md w/README.md",
-            "index ce51519..24439ed 100644",
-            "--- c/README.md",
-            "+++ w/README.md",
-            "@@ -102,6 +102,7 @@ Note: lazy loading is handled internally, so it is not required to lazy load Tru",
-            ' commit_amend_reuse_message = "A",',
-            ' commit_dry_run = "d",',
-            ' commit_no_verify = "n",',
-            -- These 3 lines have no unchanges lines between them, so they count as a single group
-            '+                    commit_instant_fixup = "F", -- Run :Trunks commit-instant-fixup',
-            "+},",
-            "+},",
-            " diff = {",
-        })
-
-        local expected = { filename = "README.md", line_nums = { 105 } }
+        local expected = {
+            { bufnr = -1, line_nums = { 3, 6, 10, 14, 26, 43, 49, 51, 58 } },
+        }
         assert.are.same(expected, result)
     end)
 end)
