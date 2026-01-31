9e2fb27d9f694b96e72b6dcb12695dc84b3d2c89
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2026-01-30 20:37:59 -0600

    chore: use builtin filepath completion

--- lua/trunks/_completion/completion.lua
+++ lua/trunks/_completion/completion.lua
@@ -15,47 +15,8 @@ function M.get_branches()
     return branches
 end
 
---- This function was copied from [mini.git](https://github.com/echasnovski/mini-git).
---- All credit for this goes to Evgeni Chasnovski and the mini.git maintainers.
---- Definitely check out [mini.nvim](https://github.com/echasnovski/mini.nvim), it's pretty sweet!
 function M._path_completion(base)
-    local cwd = vim.fn.getcwd()
-    cwd = cwd:gsub("/+$", "") .. "/"
-    local cwd_len = cwd:len()
-
-    -- List elements from (absolute) target directory
-    local target_dir = vim.fn.fnamemodify(base, ":h")
-    target_dir = (cwd .. target_dir:gsub("^%.$", "")):gsub("/+$", "") .. "/"
-    local ok, fs_entries = pcall(vim.fn.readdir, target_dir)
-    if not ok then
-        return {}
-    end
-
-    -- List directories and files separately
-    local dirs, files = {}, {}
-    for _, entry in ipairs(fs_entries) do
-        local entry_abs = target_dir .. entry
-        local arr = vim.fn.isdirectory(entry_abs) == 1 and dirs or files
-        table.insert(arr, entry_abs)
-    end
-    dirs = vim.tbl_map(function(x)
-        return x .. "/"
-    end, dirs)
-
-    -- List ordered directories first followed by ordered files
-    local order_ignore_case = function(a, b)
-        return a:lower() < b:lower()
-    end
-    table.sort(dirs, order_ignore_case)
-    table.sort(files, order_ignore_case)
-
-    -- Return candidates relative to command's cwd
-    local all = dirs
-    vim.list_extend(all, files)
-    local res = vim.tbl_map(function(x)
-        return x:sub(cwd_len + 1)
-    end, all)
-    return res, "path"
+    return vim.fn.getcompletion(base, "file")
 end
 
 ---@param command string
