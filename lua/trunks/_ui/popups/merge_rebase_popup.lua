8b74813b0d61947f7f65dd33f3891934a66cc52e
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2026-03-12 15:49:49 -0500

    fix: merge/rebase from origin grabs HEAD branch

--- lua/trunks/_ui/popups/merge_rebase_popup.lua
+++ lua/trunks/_ui/popups/merge_rebase_popup.lua
@@ -6,6 +6,20 @@ function M.render(branch_name)
     local descriptions = require("trunks._constants.keymap_descriptions").long_descriptions.merge_rebase_popup
     local run_cmd = require("trunks._core.run_cmd")
 
+    local remote_output, remote_err =
+        run_cmd.run_cmd("config --get branch." .. branch_name .. ".remote", { no_pager = true })
+    local remote = (remote_err == 0 and remote_output[1]) or "origin"
+
+    local head_output, head_err =
+        run_cmd.run_cmd("symbolic-ref refs/remotes/" .. remote .. "/HEAD", { no_pager = true })
+    local remote_ref
+    if head_err == 0 and head_output[1] then
+        local head_branch = head_output[1]:match("refs/remotes/" .. remote .. "/(.+)")
+        remote_ref = remote .. "/" .. (head_branch or branch_name)
+    else
+        remote_ref = remote .. "/" .. branch_name
+    end
+
     ---@type trunks.PopupColumn[]
     local columns = {
         {
@@ -28,20 +42,20 @@ function M.render(branch_name)
             },
         },
         {
-            title = "From remote (origin/" .. branch_name .. ")",
+            title = "From remote (" .. remote_ref .. ")",
             rows = {
                 {
                     keys = keymaps.merge_remote,
                     description = descriptions.merge_remote,
                     action = function()
-                        run_cmd.run_hidden_cmd("merge origin/" .. branch_name, { rerender = true })
+                        run_cmd.run_hidden_cmd("merge " .. remote_ref, { rerender = true })
                     end,
                 },
                 {
                     keys = keymaps.rebase_remote,
                     description = descriptions.rebase_remote,
                     action = function()
-                        run_cmd.run_hidden_cmd("rebase origin/" .. branch_name, { rerender = true })
+                        run_cmd.run_hidden_cmd("rebase " .. remote_ref, { rerender = true })
                     end,
                 },
             },
